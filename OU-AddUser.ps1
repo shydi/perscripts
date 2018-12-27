c50e0037c0949f76c96544cded3a8a406a72ec9f
Grell <sgrell3@ctisl.gtri.org>
2018-12-27 01:10:11 -0500

    a

--- OU-AddUser.ps1
+++ OU-AddUser.ps1
@@ -2,12 +2,14 @@
 .SYNOPSIS
 Adds a user to an Active Directory OU (Orginazational Unit)
 
-.SYNTAX
+.PARAMETER OU
+This paramter will have OUs as the preset values. 
 
 
 .DESCRIPTION
-This 
+This script takes inputs for the username and OU and place place the user in the designated OU. 
 
 
-
+.EXAMPLE
+OU-AddUser -OU XYZ -User flast -Verbose
 #>
